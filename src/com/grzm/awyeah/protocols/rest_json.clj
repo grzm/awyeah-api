abbfc1e4abeb80f52021c6b364f99c9bfab28add
Michael Glaesemann <grzm@seespotcode.net>
2022-10-08 10:18:42 -0500

    ask AWS to send us json (not hal+json)
    Port fa12daab9f1dd4667da02dcafef7fc54ce411e59 from aws-api

--- src/com/grzm/awyeah/protocols/rest_json.clj
+++ src/com/grzm/awyeah/protocols/rest_json.clj
@@ -4,9 +4,7 @@
 (ns ^:skip-wiki com.grzm.awyeah.protocols.rest-json
   "Impl, don't call directly."
   (:require
-   [clojure.string :as str]
    [com.grzm.awyeah.client :as client]
-   [com.grzm.awyeah.json :as json]
    [com.grzm.awyeah.protocols.common :as common]
    [com.grzm.awyeah.protocols.rest :as rest]
    [com.grzm.awyeah.shape :as shape]
@@ -40,32 +38,10 @@
   [service op-map]
   (rest/build-http-request service op-map serialize))
 
-(defmulti parser (fn [http-response] (get-in http-response [:headers "content-type"])))
-
-(defmethod parser :default [_] shape/json-parse)
-
-(defmethod parser "application/hal+json" [_]
-  (fn [shape body-str]
-    (when-not (str/blank? body-str)
-      (let [data (json/read-str body-str)]
-        (->> (into (dissoc data :_embedded :_links)
-                   (some->> (get data :_embedded)
-                            (reduce-kv (fn [m k v]
-                                         (assoc m
-                                                k
-                                                (if (sequential? v)
-                                                  (mapv #(dissoc % :_links) v)
-                                                  (dissoc v :_links))))
-                                       {})))
-             ;; TODO (dchelimsky 2019-01-09) using json-parse* to
-             ;; avoid str -> data -> str -> data, but maybe that fn
-             ;; needs a diff name?
-             (shape/json-parse* shape))))))
-
 (defmethod client/parse-http-response "rest-json"
   [service op-map http-response]
   (rest/parse-http-response service
                             op-map
                             http-response
-                            (parser http-response)
+                            shape/json-parse
                             common/json-parse-error))
