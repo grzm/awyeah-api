988775200d6f08a4608e9e6a3fc7967782b69f8b
Michael Glaesemann <grzm@seespotcode.net>
2022-10-08 15:17:53 -0500

    format
    Port 6433b188a212e3b7800babd10cc08162cc757c91 from aws-api

--- src/com/grzm/awyeah/util.clj
+++ src/com/grzm/awyeah/util.clj
@@ -181,7 +181,7 @@
       (print (str "<" (name (:tag e))))
       (when (:attrs e)
         (doseq [attr (:attrs e)]
-          (print (str " " (name (key attr)) "=\"" (val attr)"\""))))
+          (print (str " " (name (key attr)) "=\"" (val attr) "\""))))
       (if-not (empty? (:content e))
         (do
           (print ">")
