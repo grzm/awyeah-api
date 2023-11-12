8cc61b530ed2a1c648c2e67742388768b3b1b720
Michael Glaesemann <grzm@seespotcode.net>
2023-11-11 19:59:42 -0600

    [aws-api] add fn to instrument test-double client after init
    Port c42dfb0cd9bb672b4dec20b22f3ace7c01fb7afb from aws-api

--- src/com/grzm/awyeah/client/test_double.clj
+++ src/com/grzm/awyeah/client/test_double.clj
@@ -17,12 +17,16 @@
    :cognitect.anomalies/message  "No handler or response provided for op"
    :op op})
 
+(defprotocol TestDoubleClient
+  (-instrument [c ops]))
+
 (defrecord Client [info handlers]
   client.protocol/Client
   (-get-info [_] info)
 
   (-invoke [this {:keys [op request] :as op-map}]
-    (let [spec (validation/request-spec (:service info) op)]
+    (let [spec (validation/request-spec (:service info) op)
+          handler (get @handlers op)]
       (cond
         (not (get-in info [:service :operations op]))
         (validation/unsupported-op-anomaly (:service info) op)
@@ -32,12 +36,11 @@
              (not (validation/valid? spec request)))
         (validation/invalid-request-anomaly spec request)
 
-        (not (get handlers op))
+        (not handler)
         (no-handler-provided-anomaly op)
 
         :else
-        (let [handler (get handlers op)]
-          (handler op-map)))))
+        (handler op-map))))
 
   (-invoke-async [this {:keys [ch] :as op-map}]
     (let [response-chan (or ch (a/promise-chan))]
@@ -46,38 +49,54 @@
           (a/>! response-chan resp)))
       response-chan))
 
-  (-stop [_aws-client]))
+  (-stop [_aws-client])
+
+  TestDoubleClient
+  (-instrument [client ops]
+    (swap! (:handlers client)
+           (fn [handlers]
+             (reduce-kv
+               (fn [m op handler]
+                 (when-not (some-> client :service :operations op)
+                   (throw (ex-info "Operation not supported"
+                                   (validation/unsupported-op-anomaly (-> client :service) op))))
+                 (assoc m op (if (fn? handler) handler (constantly handler))))
+               handlers
+               ops)))))
 
 ;; ->Client is intended for internal use
 (alter-meta! #'->Client assoc :skip-wiki true)
+(alter-meta! #'TestDoubleClient assoc :skip-wiki true)
+
+(defn instrument
+  "Given a test double client and a `:ops` map of operations to handlers,
+   instruments the client with handlers. See `client` for more info about
+   `:ops`."
+  [client ops]
+  (-instrument client ops))
 
 (defn client
-  "Given a map with :api and :ops, returns a test client that you can
-  pass to `com.grzm.awyeah.client.api/invoke` and
+  "Given a map with :api and :ops (optional), returns a test double
+  client that you can pass to `com.grzm.awyeah.client.api/invoke` and
   `com.grzm.awyeah.client.api/stop` in implementation code.
 
-  :ops should be a map of operation to one of
-  - handler function of op-map that returns a response map
-  - literal response map
+  You can provide :ops on creation or use `instrument` to add them later.
+
+  :ops should be a map of operation (keyword) to one of
+  - a function of op-map that returns a response map
+  - a literal response map
 
   Notes:
-  - you must declare every op that will be invoked during a test
+  - you must instrument every op that will be invoked during a test
   - every op must be supported
-    - See (keys (com.grzm.awyeah.client.api/ops test-client))
+    - See (keys (com.grzm.awyeah.client.api/ops <test-client>))
   - will validate request payloads passed to `invoke` by default
     - you can disable request validation with (com.grzm.awyeah.client.api/validate-requests client false)
   - will not validate response payloads"
   [{:keys [api ops]}]
   (let [service (service/service-description (name api))]
-    (-> (->Client {:service service
-                   :validate-requests? (atom true)}
-                  (reduce-kv
-                    (fn [m op response]
-                      (when-not (some-> service :operations op)
-                        (throw (ex-info "Operation not supported"
-                                        (validation/unsupported-op-anomaly service op))))
-                      (assoc m op (if (fn? response) response (constantly response))))
-                    {}
-                    ops))
-        (assoc :api (-> service :metadata :cognitect.aws/service-name)
-               :service service))))
+    (doto (-> (->Client {:service service
+                         :validate-requests? (atom true)} (atom {}))
+              (assoc :api (-> service :metadata :cognitect.aws/service-name)
+                     :service service))
+      (instrument ops))))
